1265c801704569fb0541cd344611c0c4bf6c2d36
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-06-15 17:55:55 -0500

    Refactor: separate connection for each query

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
 import psycopg2
+import sys
 
 
-def find_popular_articles(cur):
+def connect(database_name="news"):
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    try:
+        conn = psycopg2.connect("dbname={}".format(database_name))
+        cur = conn.cursor()
+        return conn, cur
+    except psycopg2.Error as e:
+        print "Unable to connect to database"
+        sys.exit(1)
+
+
+def find_popular_articles():
     try:
         popular_article_query = '''
         SELECT articles.title, articles.id AS art_id, count(*) AS num
@@ -13,8 +25,11 @@ def find_popular_articles(cur):
         ORDER BY num DESC
         LIMIT 3;
         '''
+        conn, cur = connect()
         cur.execute(popular_article_query)
         rows = cur.fetchall()
+        cur.close()
+        conn.close()
         print("Top 3 most popular articles!")
         for row in rows:
             print('{} -- {} views'.format(row[0], row[2]))
@@ -23,7 +38,7 @@ def find_popular_articles(cur):
         print("I am unable to find_popular_articles")
 
 
-def find_popular_authors(cur):
+def find_popular_authors():
     try:
         popular_author_query = '''
         SELECT authors.name, authors.id AS aid, count(*) AS num
@@ -33,8 +48,11 @@ def find_popular_authors(cur):
         GROUP BY aid
         ORDER BY num DESC;
         '''
+        conn, cur = connect()
         cur.execute(popular_author_query)
         rows = cur.fetchall()
+        cur.close()
+        conn.close()
         print("\nAuthors ranked by popularity:")
         for row in rows:
             print('{} -- {} views'.format(row[0], row[2]))
@@ -42,15 +60,18 @@ def find_popular_authors(cur):
         print("I am unable to find_popular_authors")
 
 
-def find_high_error_dates(cur):
+def find_high_error_dates():
     try:
         greater_error_dates_query = '''
         SELECT to_char(b.date, 'FMMonth dd, yyyy'), 1.0 * b.bad_count/t.total_count
         FROM bad_view b, total_view t
         WHERE b.date = t.date AND 1.0 * b.bad_count/t.total_count > 0.01;
         '''
+        conn, cur = connect()
         cur.execute(greater_error_dates_query)
         rows = cur.fetchall()
+        cur.close()
+        conn.close()
         print("\nDates with access error rates greater than 1.00%:")
         for row in rows:
             print('{} -- {:.2%} errors'.format(row[0], row[1]))
@@ -59,9 +80,6 @@ def find_high_error_dates(cur):
 
 
 if __name__ == '__main__':
-    conn = psycopg2.connect("dbname='news'")
-    cur = conn.cursor()
-    find_popular_articles(cur)
-    find_popular_authors(cur)
-    find_high_error_dates(cur)
-    cur.close()
+    find_popular_articles()
+    find_popular_authors()
+    find_high_error_dates()
