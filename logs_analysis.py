3a149c95bdb7b029173f0ef00ceecc09a4e3c301
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-06-15 20:14:49 -0500

    Refactor: into one function with query_string and exception message as arguments

--- logs_analysis.py
+++ logs_analysis.py
@@ -15,71 +15,59 @@ def connect(database_name="news"):
         sys.exit(1)
 
 
-def find_popular_articles():
+popular_articles_query = '''
+    SELECT articles.title, articles.id AS art_id, count(*) AS num
+    FROM log, articles
+    WHERE log.path = ('/article/' || articles.slug)
+    GROUP BY art_id
+    ORDER BY num DESC
+    LIMIT 3;
+    '''
+popular_articles_exception_msg = "I am unable to find_popular_articles"
+
+
+popular_authors_query = '''
+    SELECT authors.name, authors.id AS aid, count(*) AS num
+    FROM articles, authors, log
+    WHERE articles.author=authors.id
+        AND log.path = ('/article/' || articles.slug)
+    GROUP BY aid
+    ORDER BY num DESC;
+    '''
+popular_authors_exception_msg = "I am unable to find_popular_authors"
+
+
+greater_error_dates_query = '''
+    SELECT to_char(b.date, 'FMMonth dd, yyyy'), 1.0 * b.bad_count/t.total_count
+    FROM bad_view b, total_view t
+    WHERE b.date = t.date AND 1.0 * b.bad_count/t.total_count > 0.01;
+    '''
+greater_error_dates_exception_msg = "I am unable to find_high_error_dates"
+
+
+def do_query(query_string, exception_message_string):
     try:
-        popular_article_query = '''
-        SELECT articles.title, articles.id AS art_id, count(*) AS num
-        FROM log, articles
-        WHERE log.path = ('/article/' || articles.slug)
-        GROUP BY art_id
-        ORDER BY num DESC
-        LIMIT 3;
-        '''
         conn, cur = connect()
-        cur.execute(popular_article_query)
+        cur.execute(query_string)
         rows = cur.fetchall()
         cur.close()
         conn.close()
-        print("Top 3 most popular articles!")
         for row in rows:
-            print('{} -- {} views'.format(row[0], row[2]))
+            if query_string == popular_articles_query:
+                print('{} -- {} views'.format(row[0], row[2]))
+            elif query_string == popular_authors_query:
+                print('{} -- {} views'.format(row[0], row[2]))
+            elif query_string == greater_error_dates_query:
+                print('{} -- {:.2%} errors'.format(row[0], row[1]))
 
     except BaseException:
-        print("I am unable to find_popular_articles")
-
-
-def find_popular_authors():
-    try:
-        popular_author_query = '''
-        SELECT authors.name, authors.id AS aid, count(*) AS num
-        FROM articles, authors, log
-        WHERE articles.author=authors.id
-            AND log.path = ('/article/' || articles.slug)
-        GROUP BY aid
-        ORDER BY num DESC;
-        '''
-        conn, cur = connect()
-        cur.execute(popular_author_query)
-        rows = cur.fetchall()
-        cur.close()
-        conn.close()
-        print("\nAuthors ranked by popularity:")
-        for row in rows:
-            print('{} -- {} views'.format(row[0], row[2]))
-    except BaseException:
-        print("I am unable to find_popular_authors")
-
-
-def find_high_error_dates():
-    try:
-        greater_error_dates_query = '''
-        SELECT to_char(b.date, 'FMMonth dd, yyyy'), 1.0 * b.bad_count/t.total_count
-        FROM bad_view b, total_view t
-        WHERE b.date = t.date AND 1.0 * b.bad_count/t.total_count > 0.01;
-        '''
-        conn, cur = connect()
-        cur.execute(greater_error_dates_query)
-        rows = cur.fetchall()
-        cur.close()
-        conn.close()
-        print("\nDates with access error rates greater than 1.00%:")
-        for row in rows:
-            print('{} -- {:.2%} errors'.format(row[0], row[1]))
-    except BaseException:
-        print("I am unable to find_high_error_dates")
+        print(exception_message_string)
 
 
 if __name__ == '__main__':
-    find_popular_articles()
-    find_popular_authors()
-    find_high_error_dates()
+    print("Top 3 most popular articles!")
+    do_query(popular_articles_query, popular_articles_exception_msg)
+    print("\nAuthors ranked by popularity:")
+    do_query(popular_authors_query, popular_authors_exception_msg)
+    print("\nDates with access error rates greater than 1.00%:")
+    do_query(greater_error_dates_query, greater_error_dates_exception_msg)
